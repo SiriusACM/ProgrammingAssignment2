0dcc935e3b12c780b90b2eb6d63f07edebc40fe8
Sirius Alpha Canis Majoris <sirius.acmajoris@gmail.com>
2016-05-07 21:53:20 +0200

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the solve matrix to avoid heavy computing
+## Should be a square matrix
 
-## Write a short comment describing this function
+## The first function makeCacheMatrix creates a special "matrix",
+## which is really a list containing a function to
+##
+##    set the data of the matrix
+##    get the data of the matrix
+##    set the data of the solve
+##    get the data of the solve
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        solv <- NULL
+        set <- function(mprim = matrix()) {
+                m <<- mprim
+                solv <<- NULL
+        }
+        get <- function() m
+        setsolve <- function(solve) solv <<- solve
+        getsolve <- function() solv
+        list(set = set,
+			 get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
+## Return the solve matrix cache; Compute it if needeed
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        solv <- m$getsolve()
+        if(!is.null(solv)) {
+                message("getting cached data")
+                return(solv)
+        }
+        data <- m$get()
+        solv <- solve(data)
+        m$setsolve(solv)
+        solv
 }
